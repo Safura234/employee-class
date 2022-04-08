e92a7d68b8d4503c819711d836115bc08ad04b32
Safura234 <safuramg@code.edu.az>
2022-04-08 20:18:46 +0400

    https://github.com/Safura234/employee-class.git

---
+++ employee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int search_Id;
+             {
+                Console.Clear();
+                Console.WriteLine("**************************EMPLOYEE MANAGEMENT SYSTEM MENU******************************");
+                Console.WriteLine("1. Add  an Employee");
+                Console.WriteLine("2. View Employee details");
+                Console.WriteLine("3. Search Employee details");
+                Console.WriteLine("4. Modify Employee details");
+                Console.WriteLine("5. Remove Employee details");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("----------------------------------------------------------------------------------------");
+                Console.Write("Enter Your Choise Here:-");
+                int choose_number = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            }
+    }
+    }
+
---
+++ employee/employee class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace employee
+{
+    internal class employee_class
+    {
+        private int _empID;
+        private string _name;
+        private int _salary;
+
+    }
+    class Department
+    {
+        private int _id;
+        private string _name;
+        private string _Employeelist;
+
+    }
+    private int AddEmployee(int _empID, string _name, int _salary)
+    {
+        _empID = 3;
+        _name = "Safura";
+        _salary = 800;
+        string path = @"C:\Users\Spectre\OneDrive\Bureau\employee class\database_json";
+        String dirpath = Directory.GetCurrentDirectory();
+        StreamWriter file = new StreamWriter(_name);
+
+        if (!File.Exists(dirpath + @"\" + _name))
+            file.Write("FILE DOES NOT EXIST AND HAS BEEN CREATED");
+        else
+            file.Write("FILE EXISTS");
+
+        file.Close();
+
+    }
+    public class Menu
+    {
+        public int emp_Id { get; set; }
+        public string emp_Name { get; set; }
+        public int emp_Salary { get; set; }
+        public string emp_List { get; set; }
+    }
+
+}
+    
+}
